523bee11c6a6f101f6f9b963ea6c0a2721ba4bcb
hechangmin <hechangmin@gmail.com>
2014-08-27 15:30:32 +0800

    jshint checked

--- demo.js
+++ demo.js
@@ -12,29 +12,21 @@ $(document).ready(function() {
 		switch (appCache.status) {
 			case appCache.UNCACHED: // UNCACHED == 0 
 				return 'UNCACHED';
-				break;
 			case appCache.IDLE: // IDLE == 1 
 				return 'IDLE';
-				break;
 			case appCache.CHECKING: // CHECKING == 2 
 				return 'CHECKING';
-				break;
 			case appCache.DOWNLOADING: // DOWNLOADING == 3 
 				return 'DOWNLOADING';
-				break;
 			case appCache.UPDATEREADY: // UPDATEREADY == 4 
-
 				appCache.swapCache();
-
 				return 'UPDATEREADY';
-				break;
 			case appCache.OBSOLETE: // OBSOLETE == 5 
 				return 'OBSOLETE';
-				break;
 			default:
 				return 'UKNOWN CACHE STATUS';
-				break;
-		};
+				
+		}
 	}
 
 
